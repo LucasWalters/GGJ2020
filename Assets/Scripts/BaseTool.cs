47bbf280b75d9fb6e22514c353b70314d7ef9fb6
Robin Graaf <robin_graaf@hotmail.com>
2020-01-31 22:58:40 +0100

    Basic tool with events

---
+++ Assets/Scripts/BaseTool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BaseTool : MonoBehaviour
+{
+    public UnityEvent eventToTrigger;
+    public string tagToMatch;
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == tagToMatch)
+        {
+            eventToTrigger.Invoke();
+        }
+    }
+}
