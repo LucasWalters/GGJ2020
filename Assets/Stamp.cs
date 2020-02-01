fee5b10141ee5e55839ce72c9f4bb1dd692151fa
Lucas Walters <lucas.walters@capgemini.com>
2020-02-01 18:44:34 +0100

    Stamps are now object pooled

--- Assets/Stamp.cs
+++ Assets/Stamp.cs
@@ -6,7 +6,20 @@ public class Stamp : MonoBehaviour
 {
     public GameObject stampToClone;
     public float stampCooldown = 1f;
+    public int maxStamps = 100;
+    private GameObject[] stamps;
+    private int currentStampIndex = 0;
     private float lastStamp;
+
+    private void Start()
+    {
+        stamps = new GameObject[maxStamps];
+        for (int i = 0; i < maxStamps; i++)
+        {
+            stamps[i] = Instantiate(stampToClone);
+            stamps[i].SetActive(false);
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -16,6 +29,12 @@ public class Stamp : MonoBehaviour
         }
         lastStamp = Time.time;
         ContactPoint cp = collision.contacts[0];
-        Instantiate(stampToClone, cp.point, Quaternion.LookRotation(cp.normal, stampToClone.transform.up));
+        if (++currentStampIndex == stamps.Length)
+        {
+            currentStampIndex = 0;
+        }
+        stamps[currentStampIndex].transform.position = cp.point;
+        stamps[currentStampIndex].transform.rotation = Quaternion.LookRotation(cp.normal, stampToClone.transform.up);
+        stamps[currentStampIndex].SetActive(true);
     }
 }
