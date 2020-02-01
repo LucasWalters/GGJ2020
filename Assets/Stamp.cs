0629dadbfd93efaa8e24b563a6e3ee51719f4878
Lucas Walters <lucas.walters@capgemini.com>
2020-02-01 16:17:49 +0100

    Stamp done

---
+++ Assets/Stamp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stamp : MonoBehaviour
+{
+    public GameObject stampToClone;
+    public float stampCooldown = 1f;
+    private float lastStamp;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (Time.time - lastStamp < stampCooldown)
+        {
+            return;
+        }
+        lastStamp = Time.time;
+        ContactPoint cp = collision.contacts[0];
+        Instantiate(stampToClone, cp.point, Quaternion.LookRotation(cp.normal, stampToClone.transform.up));
+    }
+}
