2d5b09b01b77866c9212e34ba9d83d0dee179f16
Lucas Walters <lucas.walters@capgemini.com>
2020-02-01 18:34:11 +0100

    Finished mirror

---
+++ Assets/MirrorLook.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorLook : MonoBehaviour
+{
+    public Transform mainCamera;
+
+    void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main.transform;
+        }
+    }
+
+    void Update()
+    {
+        Vector3 diff = (mainCamera.position - transform.position).normalized;
+        Vector3 lookDirection = new Vector3(diff.x, -diff.y, -diff.z);
+
+        transform.localRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+    }
+}
