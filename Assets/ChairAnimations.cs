069912e523e2cdd4a80e70d582bc7b772e63215b
Robin Graaf <robin_graaf@hotmail.com>
2020-02-02 11:57:49 +0100

    Added more sounds

--- Assets/ChairAnimations.cs
+++ Assets/ChairAnimations.cs
@@ -7,20 +7,32 @@ using DG.Tweening.Plugins.Options;
 
 public class ChairAnimations : MonoBehaviour
 {
-    const float DurationAnimationInSeconds = 1f;
+    const float DurationAnimationInSeconds = 0.75f;
     private Vector3 MoveInterval = (Vector3.left * 4);
     private Vector3 StartPosition = new Vector3(0, 0, 0) - (Vector3.left * 4);
     public AudioSource audioSource;
+    public GameObject chair;
 
     public void ResetChairPosition()
     {
         this.transform.position = StartPosition;
+        if (!chair.activeSelf)
+        {
+            chair.SetActive(true);
+        }
     }
     public TweenerCore<Vector3, Vector3, VectorOptions> MoveChair()
     {
         var currentPosition = this.transform.position;
 
-        //audioSource.Play();
+        if (Vector3.Distance(this.transform.position, Vector3.zero) < 1)
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+            audioSource.Play();
+        }
 
         return this.transform.DOMove(currentPosition + MoveInterval, DurationAnimationInSeconds).SetEase(Ease.InOutQuad);
     }
